b4e1dd8bb61ee96a2e619affd08d25188727c927
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-09-23 18:30:16 +0300

    create html templete

--- bordes/views.py
+++ bordes/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Board
+
+from .models import Board # import the model you want to display
+
 # Create your views here.
 
 def home(request):
     borders = Board.objects.all()
-    borderList = []
-    for border in borders:
-        borderList.append(border.name)
 
-    return HttpResponse(borderList)
+    # render the template and pass the context
+    return render(request, 'home.html', {'borders': borders}) 
