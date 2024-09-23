f53aebe28225afb81f5782c2d43c4cffc7c0f131
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-09-23 17:30:50 +0300

    - create 3 Board , Topic, Post Tables

--- bordes/models.py
+++ bordes/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 
 from django.contrib.auth.models import User
@@ -8,4 +9,24 @@ class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     decription = models.CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return self.name
 
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=50)
+    board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
+    created_at= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.subject
+
+class Post(models.Model):
+    message = models.TextField(max_length=2000)
+    Topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    created_at= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.message
--- bordes/views.py
+++ bordes/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Board
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    borders = Board.objects.all()
+    borderList = []
+    for border in borders:
+        borderList.append(border.name)
+
+    return HttpResponse(borderList)
