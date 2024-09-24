ddc57ffef618c5916c3a4c527d70c3193f89492f
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-09-24 16:03:22 +0300

    add static folder add templates files

--- bordes/views.py
+++ bordes/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 
 from .models import Board # import the model you want to display
 
 # Create your views here.
 
 def home(request):
-    borders = Board.objects.all()
+    boarders = Board.objects.all()
 
     # render the template and pass the context
-    return render(request, 'home.html', {'borders': borders}) 
+    return render(request, 'home.html', {'boarders': boarders}) 
+
+def board_topics(request, board_id):
+
+    board = get_object_or_404(Board, pk=board_id)
+    return HttpResponse(board.name)
