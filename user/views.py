2b1eddf358b5b2be498d235486ccca9a3a39ef3a
Mohamed Refaat <mohamedrefaat900@gmail.com>
2024-09-25 19:58:41 +0300

    - import Login view

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout as logoutUser
 from .form import SignupForm
 # Create your views here.
 
@@ -10,6 +11,18 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
-            redirect('home')
+            return redirect('home')
     
     return render(request, 'signup.html' , {'form': form})
+
+
+def login(request):
+    print("This user : "+ request.user.username)
+    return redirect('home')
+
+# def logout(request):
+#     logoutUser(request)
+#     return redirect('home')
+
+def password_change(request):
+    pass
